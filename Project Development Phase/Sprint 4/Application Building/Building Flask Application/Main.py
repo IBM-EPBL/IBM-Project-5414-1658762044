e13611c168780ef93eecbe5e64fa322647e5c27d
GAYATHRI M <99126859+gayumanimaran@users.noreply.github.com>
2022-11-14 19:03:21 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 4/Application Building/Building Flask Application/Main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+
+video = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = video.read()
+	cv2.imshow("Frame", frame)
+	k = cv2.waitKey(1)
+	if k == ord('q'):
+		break
+
+video.release()
+cv2.destroyAllWindows()
+
+
+# 
+
+# 
